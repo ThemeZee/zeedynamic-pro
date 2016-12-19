13a8c7a24b272185f46a2bf1d8fce3e23f741921
Lenovo-PC\thomasw <kontakt@themecoder.de>
2016-12-19 17:00:49 +0100

    bumped version and updated readme

--- zeedynamic-pro.php
+++ zeedynamic-pro.php
@@ -5,7 +5,7 @@ Plugin URI: http://themezee.com/addons/zeedynamic-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the zeeDynamic theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.0.4
+Version: 1.0.5
 Text Domain: zeedynamic-pro
 Domain Path: /languages/
 License: GPL v3
@@ -62,7 +62,7 @@ class zeeDynamic_Pro {
 		define( 'ZEE_DYNAMIC_PRO_NAME', 'zeeDynamic Pro' );
 
 		// Define Version Number.
-		define( 'ZEE_DYNAMIC_PRO_VERSION', '1.0.4' );
+		define( 'ZEE_DYNAMIC_PRO_VERSION', '1.0.5' );
 
 		// Define Plugin Name.
 		define( 'ZEE_DYNAMIC_PRO_PRODUCT_ID', 58567 );
