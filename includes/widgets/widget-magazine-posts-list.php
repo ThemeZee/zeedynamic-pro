11be695bfb77e47b77ad750437f23eac2cfafc64
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-03-03 18:10:01 +0100

    added content templates for Magazine widgets and improved caching with transients

--- includes/widgets/widget-magazine-posts-list.php
+++ includes/widgets/widget-magazine-posts-list.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Magazine Posts List Widget
+ * Magazine List Widget
  *
  * Display the latest posts from a selected category in a list layout.
  * Intented to be used in the Magazine Homepage widget area to built a magazine layouted page.
@@ -21,19 +21,13 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 		// Setup Widget.
 		parent::__construct(
 			'zeedynamic-magazine-posts-list', // ID.
-			sprintf( esc_html__( 'Magazine Posts: List (%s)', 'zeedynamic-pro' ), 'zeeDynamic Pro' ), // Name.
+			esc_html__( 'Magazine (List)', 'zeedynamic-pro' ), // Name.
 			array(
-				'classname' => 'zeedynamic_magazine_posts_list',
+				'classname' => 'zeedynamic-magazine-list-widget',
 				'description' => esc_html__( 'Displays your posts from a selected category in a simple list layout. Please use this widget ONLY in the Magazine Homepage widget area.', 'zeedynamic-pro' ),
 				'customize_selective_refresh' => true,
 			) // Args.
 		);
-
-		// Delete Widget Cache on certain actions.
-		add_action( 'save_post', array( $this, 'delete_widget_cache' ) );
-		add_action( 'deleted_post', array( $this, 'delete_widget_cache' ) );
-		add_action( 'switch_theme', array( $this, 'delete_widget_cache' ) );
-
 	}
 
 	/**
@@ -42,16 +36,12 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 	private function default_settings() {
 
 		$defaults = array(
-			'title'				=> '',
-			'category'			=> 0,
-			'number'			=> 3,
-			'meta_date'			=> true,
-			'meta_author'		=> true,
-			'meta_category'		=> true,
+			'title'    => '',
+			'category' => 0,
+			'number'   => 3,
 		);
 
 		return $defaults;
-
 	}
 
 	/**
@@ -63,22 +53,6 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 	 * @param array $instance / Settings for this widget instance.
 	 */
 	function widget( $args, $instance ) {
-
-		$cache = array();
-
-		// Get Widget Object Cache.
-		if ( ! $this->is_preview() ) {
-			$cache = wp_cache_get( 'widget_zeedynamic_magazine_posts_list', 'widget' );
-		}
-		if ( ! is_array( $cache ) ) {
-			$cache = array();
-		}
-
-		// Display Widget from Cache if exists.
-		if ( isset( $cache[ $this->id ] ) ) {
-			echo $cache[ $this->id ];
-			return;
-		}
 
 		// Start Output Buffering.
 		ob_start();
@@ -106,14 +80,8 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 		<?php
 		echo $args['after_widget'];
 
-		// Set Cache.
-		if ( ! $this->is_preview() ) {
-			$cache[ $this->id ] = ob_get_flush();
-			wp_cache_set( 'widget_zeedynamic_magazine_posts_list', $cache, 'widget' );
-		} else {
-			ob_end_flush();
-		}
-
+		// End Output Buffering.
+		ob_end_flush();
 	}
 
 	/**
@@ -128,14 +96,15 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 	 */
 	function render( $settings ) {
 
-		// Get latest posts from database.
+		// Get cached post ids.
+		$post_ids = zeedynamic_get_magazine_post_ids( $this->id, $settings['category'], $settings['number'] );
+
+		// Fetch posts from database.
 		$query_arguments = array(
-			'posts_per_page' => (int) $settings['number'],
-			'ignore_sticky_posts' => true,
-			'cat' => (int) $settings['category'],
+			'post__in'            => $post_ids,
+			'no_found_rows'       => true,
 		);
 		$posts_query = new WP_Query( $query_arguments );
-		$i = 0;
 
 		// Check if there are posts.
 		if ( $posts_query->have_posts() ) :
@@ -144,32 +113,10 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 			add_filter( 'excerpt_length', 'zeedynamic_magazine_posts_excerpt_length' );
 
 			// Display Posts.
-			while ( $posts_query->have_posts() ) : $posts_query->the_post(); ?>
+			while ( $posts_query->have_posts() ) : $posts_query->the_post();
 
-				<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?>>
+				get_template_part( 'template-parts/widgets/magazine-full-post', 'list' );
 
-					<a href="<?php esc_url( the_permalink() ); ?>" rel="bookmark">
-						<?php the_post_thumbnail( 'zeedynamic-thumbnail-archive' ); ?>
-					</a>
-
-					<header class="entry-header">
-
-						<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-						<?php $this->entry_meta( $settings ); ?>
-
-					</header><!-- .entry-header -->
-
-					<div class="entry-content clearfix">
-
-						<?php the_excerpt(); ?>
-						<?php zeedynamic_more_link(); ?>
-
-					</div><!-- .entry-content -->
-
-				</article>
-
-			<?php
 			endwhile;
 
 			// Remove excerpt filter.
@@ -179,44 +126,7 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 
 		// Reset Postdata.
 		wp_reset_postdata();
-
 	}
-
-	/**
-	 * Displays Entry Meta of Posts
-	 *
-	 * @param array $settings / Settings for this widget instance.
-	 */
-	function entry_meta( $settings ) {
-
-		$postmeta = '';
-
-		if ( true === $settings['meta_date'] ) {
-
-			$postmeta .= zeedynamic_meta_date();
-
-		}
-
-		if ( true === $settings['meta_author'] ) {
-
-			$postmeta .= zeedynamic_meta_author();
-
-		}
-
-		if ( true === $settings['meta_category'] ) {
-
-			$postmeta .= zeedynamic_meta_category();
-
-		}
-
-		if ( $postmeta ) {
-
-			echo '<div class="entry-meta">' . $postmeta . '</div>';
-
-		}
-
-	} // entry_meta()
-
 
 	/**
 	 * Displays Widget Title
@@ -240,7 +150,7 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 
 				// Display Widget Title with link to category archive.
 				echo '<div class="widget-header">';
-				echo '<h1 class="widget-title"><a class="category-archive-link" href="'. $link_url .'" title="'. $link_title . '">'. $widget_title . '</a></h1>';
+				echo '<h3 class="widget-title"><a class="category-archive-link" href="' . $link_url . '" title="' . $link_title . '">' . $widget_title . '</a></h3>';
 				echo '</div>';
 
 			else :
@@ -251,9 +161,7 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 			endif;
 
 		endif;
-
-	} // widget_title()
-
+	}
 
 	/**
 	 * Update Widget Settings
@@ -268,11 +176,8 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 		$instance['title'] = sanitize_text_field( $new_instance['title'] );
 		$instance['category'] = (int) $new_instance['category'];
 		$instance['number'] = (int) $new_instance['number'];
-		$instance['meta_date'] = ! empty( $new_instance['meta_date'] );
-		$instance['meta_author'] = ! empty( $new_instance['meta_author'] );
-		$instance['meta_category'] = ! empty( $new_instance['meta_category'] );
 
-		$this->delete_widget_cache();
+		zeedynamic_flush_magazine_post_ids();
 
 		return $instance;
 	}
@@ -290,7 +195,7 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'zeedynamic-pro' ); ?>
-				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $settings['title']; ?>" />
+				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $settings['title'] ); ?>" />
 			</label>
 		</p>
 
@@ -311,41 +216,10 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number of posts:', 'zeedynamic-pro' ); ?>
-				<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $settings['number']; ?>" size="3" />
+				<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo absint( $settings['number'] ); ?>" size="3" />
 			</label>
 		</p>
 
-		<p>
-			<label for="<?php echo $this->get_field_id( 'meta_date' ); ?>">
-				<input class="checkbox" type="checkbox" <?php checked( $settings['meta_date'] ); ?> id="<?php echo $this->get_field_id( 'meta_date' ); ?>" name="<?php echo $this->get_field_name( 'meta_date' ); ?>" />
-				<?php esc_html_e( 'Display post date', 'zeedynamic-pro' ); ?>
-			</label>
-		</p>
-
-		<p>
-			<label for="<?php echo $this->get_field_id( 'meta_author' ); ?>">
-				<input class="checkbox" type="checkbox" <?php checked( $settings['meta_author'] ); ?> id="<?php echo $this->get_field_id( 'meta_author' ); ?>" name="<?php echo $this->get_field_name( 'meta_author' ); ?>" />
-				<?php esc_html_e( 'Display post author', 'zeedynamic-pro' ); ?>
-			</label>
-		</p>
-
-		<p>
-			<label for="<?php echo $this->get_field_id( 'meta_category' ); ?>">
-				<input class="checkbox" type="checkbox" <?php checked( $settings['meta_category'] ); ?> id="<?php echo $this->get_field_id( 'meta_category' ); ?>" name="<?php echo $this->get_field_name( 'meta_category' ); ?>" />
-				<?php esc_html_e( 'Display post category', 'zeedynamic-pro' ); ?>
-			</label>
-		</p>
-
-	<?php
-	} // form()
-
-
-	/**
-	 * Delete Widget Cache
-	 */
-	public function delete_widget_cache() {
-
-		wp_cache_delete( 'widget_zeedynamic_magazine_posts_list', 'widget' );
-
+		<?php
 	}
 }
