a97c053a58fda3932471dd3f867bb1193009332a
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-06-17 16:35:46 +0200

    ignore sticky posts in Magazine widgets

--- includes/widgets/widget-magazine-posts-list.php
+++ includes/widgets/widget-magazine-posts-list.php
@@ -109,9 +109,10 @@ class zeeDynamic_Pro_Magazine_Posts_List_Widget extends WP_Widget {
 
 		// Fetch posts from database.
 		$query_arguments = array(
-			'post__in'       => $post_ids,
-			'posts_per_page' => absint( $settings['number'] ),
-			'no_found_rows'  => true,
+			'post__in'            => $post_ids,
+			'posts_per_page'      => absint( $settings['number'] ),
+			'ignore_sticky_posts' => true,
+			'no_found_rows'       => true,
 		);
 		$posts_query = new WP_Query( $query_arguments );
 
--- includes/widgets/widget-magazine-posts-single.php
+++ includes/widgets/widget-magazine-posts-single.php
@@ -108,9 +108,10 @@ class zeeDynamic_Pro_Magazine_Posts_Single_Widget extends WP_Widget {
 
 		// Fetch posts from database.
 		$query_arguments = array(
-			'post__in'       => $post_id,
-			'posts_per_page' => 1,
-			'no_found_rows'  => true,
+			'post__in'            => $post_id,
+			'posts_per_page'      => 1,
+			'ignore_sticky_posts' => true,
+			'no_found_rows'       => true,
 		);
 		$posts_query = new WP_Query( $query_arguments );
 
