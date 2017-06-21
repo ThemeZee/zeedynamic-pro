143c0f0afc5c1c43ed384dd9f0967abbfead9101
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-06-21 15:12:20 +0200

    fixed footer line to show local time

--- includes/modules/class-footer-line.php
+++ includes/modules/class-footer-line.php
@@ -55,7 +55,7 @@ class zeeDynamic_Pro_Footer_Line {
 
 			echo '<nav id="footer-navigation" class="footer-navigation navigation clearfix" role="navigation">';
 
-			echo '<span class="today">' . date( get_option( 'date_format' ) . ' / ' . get_option( 'time_format' ) ) . '</span>';
+			echo '<span class="today">' . current_time( get_option( 'date_format' ) . ' / ' . get_option( 'time_format' ) ) . '</span>';
 
 			wp_nav_menu( array(
 				'theme_location' => 'footer',
