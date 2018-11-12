efb831d02a9342eefdbfcc2efe913c467b712406
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-11-12 11:28:27 +0100

    updated color settings to set primary color for Gutenberg

--- includes/modules/class-custom-colors.php
+++ includes/modules/class-custom-colors.php
@@ -8,7 +8,9 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Custom Colors Class
@@ -30,9 +32,11 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Custom Color CSS code to custom stylesheet output.
 		add_filter( 'zeedynamic_pro_custom_css_stylesheet', array( __CLASS__, 'custom_colors_css' ) );
 
+		// Add Custom Color CSS code to the Gutenberg editor.
+		add_action( 'enqueue_block_editor_assets', array( __CLASS__, 'custom_editor_colors_css' ) );
+
 		// Add Custom Color Settings.
 		add_action( 'customize_register', array( __CLASS__, 'color_settings' ) );
-
 	}
 
 	/**
@@ -58,8 +62,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.top-navigation-menu ul {
 					background: ' . $theme_options['top_navi_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		// Set Primary Navigation Color.
@@ -75,8 +78,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.main-navigation-toggle:active {
 					background: ' . $theme_options['navi_primary_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		// Set Secondary Navigation Color.
@@ -90,8 +92,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.main-navigation-menu ul {
 					background: ' . $theme_options['navi_secondary_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		// Set Primary Content Color.
@@ -104,7 +105,8 @@ class zeeDynamic_Pro_Custom_Colors {
 				a:visited,
 				.site-title,
 				.site-title a:link,
-				.site-title a:visited {
+				.site-title a:visited,
+				.has-primary-color {
 					color: ' . $theme_options['content_primary_color'] . ';
 				}
 
@@ -168,8 +170,11 @@ class zeeDynamic_Pro_Custom_Colors {
 				.tzwb-social-icons .social-icons-menu li a:active {
 				    background: #333333;
 				}
-				';
 
+				.has-primary-background-color {
+					background-color: ' . $theme_options['content_primary_color'] . ';
+				}
+			';
 		}
 
 		// Set Link Color.
@@ -242,8 +247,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.tzwb-tabbed-content .tzwb-tabnavi li a.current-tab {
 				    background: #e84747;
 				}
-				';
-
+			';
 		}
 
 		// Set Primary Hover Content Color.
@@ -263,8 +267,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.tzwb-tabbed-content .tzwb-tabnavi li a.current-tab {
 					background: ' . $theme_options['content_primary_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		// Set Footer Widgets Color.
@@ -277,8 +280,7 @@ class zeeDynamic_Pro_Custom_Colors {
 				.footer-widgets-background {
 					background: ' . $theme_options['footer_area_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		// Set Footer Line Color.
@@ -290,12 +292,60 @@ class zeeDynamic_Pro_Custom_Colors {
 				.footer-navigation {
 					background: ' . $theme_options['footer_navi_color'] . ';
 				}
-				';
-
+			';
 		}
 
 		return $custom_css;
+	}
 
+	/**
+	 * Adds Color CSS styles in the Gutenberg Editor to override default colors
+	 *
+	 * @return void
+	 */
+	static function custom_editor_colors_css() {
+
+		// Get Theme Options from Database.
+		$theme_options = zeeDynamic_Pro_Customizer::get_theme_options();
+
+		// Get Default Fonts from settings.
+		$default_options = zeeDynamic_Pro_Customizer::get_default_options();
+
+		// Set Primary Color.
+		if ( $theme_options['content_primary_color'] !== $default_options['content_primary_color'] ) {
+
+			$custom_css = '
+				.has-primary-color,
+				.edit-post-visual-editor .editor-block-list__block a {
+					color: ' . $theme_options['content_primary_color'] . ';
+				}
+				.has-primary-background-color {
+					background-color: ' . $theme_options['content_primary_color'] . ';
+				}
+			';
+
+			wp_add_inline_style( 'zeedynamic-editor-styles', $custom_css );
+		}
+	}
+
+	/**
+	 * Change primary color in Gutenberg Editor.
+	 *
+	 * @return array $editor_settings
+	 */
+	static function change_primary_color( $color ) {
+		// Get Theme Options from Database.
+		$theme_options = zeeDynamic_Pro_Customizer::get_theme_options();
+
+		// Get Default Fonts from settings.
+		$default_options = zeeDynamic_Pro_Customizer::get_default_options();
+
+		// Set Primary Color.
+		if ( $theme_options['content_primary_color'] !== $default_options['content_primary_color'] ) {
+			$color = $theme_options['content_primary_color'];
+		}
+
+		return $color;
 	}
 
 	/**
@@ -309,9 +359,8 @@ class zeeDynamic_Pro_Custom_Colors {
 		$wp_customize->add_section( 'zeedynamic_pro_section_colors', array(
 			'title'    => __( 'Theme Colors', 'zeedynamic-pro' ),
 			'priority' => 60,
-			'panel' => 'zeedynamic_options_panel',
-			)
-		);
+			'panel'    => 'zeedynamic_options_panel',
+		) );
 
 		// Get Default Colors from settings.
 		$default_options = zeeDynamic_Pro_Customizer::get_default_options();
@@ -319,16 +368,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Top Navigation Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[top_navi_color]', array(
 			'default'           => $default_options['top_navi_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'postMessage',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[top_navi_color]', array(
-				'label'      => _x( 'Top Navigation', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[top_navi_color]',
+				'label'    => _x( 'Top Navigation', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[top_navi_color]',
 				'priority' => 1,
 			)
 		) );
@@ -336,16 +384,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Navigation Primary Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[navi_primary_color]', array(
 			'default'           => $default_options['navi_primary_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[navi_primary_color]', array(
-				'label'      => _x( 'Navigation (primary)', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[navi_primary_color]',
+				'label'    => _x( 'Navigation (primary)', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[navi_primary_color]',
 				'priority' => 2,
 			)
 		) );
@@ -353,16 +400,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Navigation Secondary Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[navi_secondary_color]', array(
 			'default'           => $default_options['navi_secondary_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[navi_secondary_color]', array(
-				'label'      => _x( 'Navigation (secondary)', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[navi_secondary_color]',
+				'label'    => _x( 'Navigation (secondary)', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[navi_secondary_color]',
 				'priority' => 3,
 			)
 		) );
@@ -370,16 +416,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Post Primary Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[content_primary_color]', array(
 			'default'           => $default_options['content_primary_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[content_primary_color]', array(
-				'label'      => _x( 'Content (primary)', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[content_primary_color]',
+				'label'    => _x( 'Content (primary)', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[content_primary_color]',
 				'priority' => 4,
 			)
 		) );
@@ -387,16 +432,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Link and Button Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[content_secondary_color]', array(
 			'default'           => $default_options['content_secondary_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'refresh',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[content_secondary_color]', array(
-				'label'      => _x( 'Content (secondary)', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[content_secondary_color]',
+				'label'    => _x( 'Content (secondary)', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[content_secondary_color]',
 				'priority' => 5,
 			)
 		) );
@@ -404,16 +448,15 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Footer Widgets Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[footer_area_color]', array(
 			'default'           => $default_options['footer_area_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'postMessage',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[footer_area_color]', array(
-				'label'      => _x( 'Footer', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[footer_area_color]',
+				'label'    => _x( 'Footer', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[footer_area_color]',
 				'priority' => 7,
 			)
 		) );
@@ -421,22 +464,21 @@ class zeeDynamic_Pro_Custom_Colors {
 		// Add Footer Line Color setting.
 		$wp_customize->add_setting( 'zeedynamic_theme_options[footer_navi_color]', array(
 			'default'           => $default_options['footer_navi_color'],
-			'type'           	=> 'option',
+			'type'              => 'option',
 			'transport'         => 'postMessage',
 			'sanitize_callback' => 'sanitize_hex_color',
-			)
-		);
+		) );
 		$wp_customize->add_control( new WP_Customize_Color_Control(
 			$wp_customize, 'zeedynamic_theme_options[footer_navi_color]', array(
-				'label'      => _x( 'Footer Navigation', 'color setting', 'zeedynamic-pro' ),
-				'section'    => 'zeedynamic_pro_section_colors',
-				'settings'   => 'zeedynamic_theme_options[footer_navi_color]',
-				'priority' 	=> 8,
+				'label'    => _x( 'Footer Navigation', 'color setting', 'zeedynamic-pro' ),
+				'section'  => 'zeedynamic_pro_section_colors',
+				'settings' => 'zeedynamic_theme_options[footer_navi_color]',
+				'priority' => 8,
 			)
 		) );
-
 	}
 }
 
 // Run Class.
 add_action( 'init', array( 'zeeDynamic_Pro_Custom_Colors', 'setup' ) );
+add_filter( 'zeedynamic_primary_color', array( 'zeeDynamic_Pro_Custom_Colors', 'change_primary_color' ) );
--- zeedynamic-pro.php
+++ zeedynamic-pro.php
@@ -5,11 +5,11 @@ Plugin URI: http://themezee.com/addons/zeedynamic-pro/
 Description: Adds additional features like custom colors, google fonts, widget areas and footer copyright to the zeeDynamic theme.
 Author: ThemeZee
 Author URI: https://themezee.com/
-Version: 1.4.1
+Version: 1.5
 Text Domain: zeedynamic-pro
 Domain Path: /languages/
-License: GPL v3
-License URI: http://www.gnu.org/licenses/gpl-3.0.html
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 zeeDynamic Pro
 Copyright(C) 2016, ThemeZee.com - support@themezee.com
@@ -17,7 +17,9 @@ Copyright(C) 2016, ThemeZee.com - support@themezee.com
 */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 
 /**
@@ -48,7 +50,6 @@ class zeeDynamic_Pro {
 
 		// Setup Action Hooks.
 		self::setup_actions();
-
 	}
 
 	/**
@@ -62,7 +63,7 @@ class zeeDynamic_Pro {
 		define( 'ZEE_DYNAMIC_PRO_NAME', 'zeeDynamic Pro' );
 
 		// Define Version Number.
-		define( 'ZEE_DYNAMIC_PRO_VERSION', '1.4.1' );
+		define( 'ZEE_DYNAMIC_PRO_VERSION', '1.5' );
 
 		// Define Plugin Name.
 		define( 'ZEE_DYNAMIC_PRO_PRODUCT_ID', 58567 );
@@ -78,7 +79,6 @@ class zeeDynamic_Pro {
 
 		// Plugin Root File.
 		define( 'ZEE_DYNAMIC_PRO_PLUGIN_FILE', __FILE__ );
-
 	}
 
 	/**
@@ -122,7 +122,6 @@ class zeeDynamic_Pro {
 		require_once ZEE_DYNAMIC_PRO_PLUGIN_DIR . '/includes/widgets/widget-magazine-posts-vertical-box.php';
 		require_once ZEE_DYNAMIC_PRO_PLUGIN_DIR . '/includes/widgets/widget-magazine-posts-list.php';
 		require_once ZEE_DYNAMIC_PRO_PLUGIN_DIR . '/includes/widgets/widget-magazine-posts-single.php';
-
 	}
 
 	/**
@@ -144,7 +143,6 @@ class zeeDynamic_Pro {
 
 		// Add automatic plugin updater from ThemeZee Store API.
 		add_action( 'admin_init', array( __CLASS__, 'plugin_updater' ), 0 );
-
 	}
 
 	/**
@@ -177,7 +175,6 @@ class zeeDynamic_Pro {
 
 		// Add Custom CSS.
 		wp_add_inline_style( 'zeedynamic-pro', $custom_css );
-
 	}
 
 	/**
@@ -196,7 +193,6 @@ class zeeDynamic_Pro {
 		register_widget( 'zeeDynamic_Pro_Magazine_Vertical_Box_Widget' );
 		register_widget( 'zeeDynamic_Pro_Magazine_Posts_List_Widget' );
 		register_widget( 'zeeDynamic_Pro_Magazine_Posts_Single_Widget' );
-
 	}
 
 	/**
@@ -231,16 +227,14 @@ class zeeDynamic_Pro {
 
 			// Setup the updater.
 			$zeedynamic_pro_updater = new zeeDynamic_Pro_Plugin_Updater( ZEE_DYNAMIC_PRO_STORE_API_URL, __FILE__, array(
-					'version' 	=> ZEE_DYNAMIC_PRO_VERSION,
-					'license' 	=> $license_key,
-					'item_name' => ZEE_DYNAMIC_PRO_NAME,
-					'item_id'   => ZEE_DYNAMIC_PRO_PRODUCT_ID,
-					'author' 	=> 'ThemeZee',
-				)
-			);
+				'version'   => ZEE_DYNAMIC_PRO_VERSION,
+				'license'   => $license_key,
+				'item_name' => ZEE_DYNAMIC_PRO_NAME,
+				'item_id'   => ZEE_DYNAMIC_PRO_PRODUCT_ID,
+				'author'    => 'ThemeZee',
+			) );
 
 		endif;
-
 	}
 }
 
